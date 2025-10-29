2090560ae043464b751dc72702c76f8f9f07ed02
aanchal patel <aanchalpatel357@gmail.com>
2025-10-29 17:10:50 +0530

    changes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
-export default function Page(){
-  return <p>Customer page</p>
+import CustomersTable from "@/app/ui/customers/table"
+import { fetchCustomers } from "@/app/lib/data"
+
+export default async  function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+   
+  }>;
+}){
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  return (
+    <div><CustomersTable query={query}/></div>
+  )
 }
